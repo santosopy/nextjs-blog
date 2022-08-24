a4afb8e9f4d9efae9bac6059ea093d146f27c685
santosopy <santoso@tsart.jp>
2022-08-24 14:40:51 +0900

    say

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[aaabbb Your Self Introduction.a]</p>
+        <p>[HEllo]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
