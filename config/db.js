e57e80d46c1030cfac8c234f99716b1362ade549
santosopy <santoso@tsart.jp>
2022-08-25 18:02:52 +0900

    change db config

--- config/db.js
+++ config/db.js
@@ -1,12 +1,19 @@
 import mysql from "serverless-mysql";
 
+// config: {
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   port: 3306,
+//   database: "nextdb",
+// },
+
 const pool = mysql({
   config: {
-    host: "localhost",
-    user: "root",
-    password: "root",
-    port: 3306,
-    database: "nextdb",
+    HOST: "7768zghy39a5.us-east-1.psdb.cloud",
+    USERNAME: "gz22hfjpyfrp",
+    PASSWORD: "pscale_pw_80Rvnv9V9K91TM2co5F7ljIE-Dqv5wTaF7l7jVatmIE",
+    DATABASE: "nextdb"
   },
 })
 
