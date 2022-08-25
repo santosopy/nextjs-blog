e7241747ce590358cc23040de7260e5826b88311
santosopy <santoso@tsart.jp>
2022-08-25 19:02:32 +0900

    s

--- config/db.js
+++ config/db.js
@@ -13,8 +13,8 @@ import mysql from "serverless-mysql";
 const pool = mysql({
   config: {
     HOST: "y312s3i0b6z4.us-east-4.psdb.cloud",
-    USERNAME: "d9ohwwhbytbc",
-    PASSWORD: "pscale_pw_xBIZI6C12Tkq36mKhTsIKwqsrlNYeZYoDR83tsu7fio",
+    USERNAME: "3owsugrgw2w5",
+    PASSWORD: "pscale_pw_KoHFbXfILDePXxZjH8YfQ0CIegJ8QODieeYCWfGmPZ0",
     DATABASE: "nextdb"
   }
 })
