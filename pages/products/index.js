862d64ec9b38f77658f8c0fb69673f2796a0bb1b
santosopy <santoso@tsart.jp>
2022-08-30 17:54:25 +0900

    verse

--- pages/products/index.js
+++ pages/products/index.js
@@ -23,7 +23,7 @@ export default ProductsPage;
 
 export const getServerSideProps = async () => {
   const { data: products } = await axios.get(
-    "http://localhost:3000/api/products"
+    "https://nextjs-blog-santosopy.vercel.app/api/products"
   );
 
   return {
