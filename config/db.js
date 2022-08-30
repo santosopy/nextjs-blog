733766e61ad560f33ce5ed1b33228152e0d2b0ab
santosopy <santoso@tsart.jp>
2022-08-30 15:59:49 +0900

    edit mysql

--- config/db.js
+++ config/db.js
@@ -1,20 +1,23 @@
 import mysql from "serverless-mysql";
 
-// const pool = mysql({
-//   config: {
-//     host: "localhost",
-//     user: "root",
-//     password: "root",
-//     port: 3306,
-//     database: "nextdb",
-//   },
-// })
-
 const pool = mysql({
   config: {
-    HOST: "y312s3i0b6z4.us-east-4.psdb.cloud",
-    USERNAME: "3owsugrgw2w5",
-    PASSWORD: "pscale_pw_KoHFbXfILDePXxZjH8YfQ0CIegJ8QODieeYCWfGmPZ0",
-    DATABASE: "nextdb"
-  }
+    host: "localhost",
+    user: "root",
+    password: "root",
+    port: 3306,
+    database: "productsdb",
+  },
 })
+
+
+// const pool = mysql({
+//   config: {
+//     HOST: "y312s3i0b6z4.us-east-4.psdb.cloud",
+//     USERNAME: "3owsugrgw2w5",
+//     PASSWORD: "pscale_pw_KoHFbXfILDePXxZjH8YfQ0CIegJ8QODieeYCWfGmPZ0",
+//     DATABASE: "nextdb"
+//   }
+// })
+
+export{ pool }
