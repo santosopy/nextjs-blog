e4adcdccaee01aefe401d0a3f2e5e4a2141eabf1
santosopy <santoso@tsart.jp>
2022-08-30 17:45:30 +0900

    planetscale

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
 import mysql from "serverless-mysql";
 
-const pool = mysql({
-  config: {
-    host: "localhost",
-    user: "root",
-    password: "root",
-    port: 3306,
-    database: "productsdb",
-  },
-})
-
-
 // const pool = mysql({
 //   config: {
-//     HOST: "y312s3i0b6z4.us-east-4.psdb.cloud",
-//     USERNAME: "3owsugrgw2w5",
-//     PASSWORD: "pscale_pw_KoHFbXfILDePXxZjH8YfQ0CIegJ8QODieeYCWfGmPZ0",
-//     DATABASE: "nextdb"
-//   }
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     port: 3306,
+//     database: "productsdb",
+//   },
 // })
+
+
+const pool = mysql({
+  config: {
+    HOST: "us-east.connect.psdb.cloud",
+    USERNAME: "csm73g36gr3zyzr2c9r9",
+    PASSWORD: "pscale_pw_mP8g7HSpvAjrvuwkUytcD3tPTf9Uy9QNgbMtfTlk0uG",
+    DATABASE: "productsdb",
+  },
+})
 
 export{ pool }
