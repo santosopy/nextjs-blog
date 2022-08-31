ad4e5f50539505a76d5aae08b1ce9f6e829e8f93
santosopy <santoso@tsart.jp>
2022-08-31 17:42:14 +0900

    test

--- pages/products/index.js
+++ pages/products/index.js
@@ -3,17 +3,17 @@ import { Layout } from "components/Layout";
 import { ProductCard } from "components/ProductCard";
 
 function ProductsPage({ products = [] }) {
-  const renderProducts = () => {
-    if (products.length === 0) return <h1>No Products</h1>;
-    return products.map((product) => (
-      <ProductCard key={product.id} product={product} />
-    ));
-  };
+  // const renderProducts = () => {
+  //   if (products.length === 0) return <h1>No Products</h1>;
+  //   return products.map((product) => (
+  //     <ProductCard key={product.id} product={product} />
+  //   ));
+  // };
 
   return (
     <Layout>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-4">
-        {renderProducts()}
+        test
       </div>
     </Layout>
   );
@@ -21,14 +21,14 @@ function ProductsPage({ products = [] }) {
 
 export default ProductsPage;
 
-export const getServerSideProps = async () => {
-  const { data: products } = await axios.get(
-    "https://nextjs-blog-flame-kappa-86.vercel.app/api/products"
-  );
+// export const getServerSideProps = async () => {
+//   const { data: products } = await axios.get(
+//     "https://nextjs-blog-flame-kappa-86.vercel.app/api/products"
+//   );
 
-  return {
-    props: {
-      products,
-    },
-  };
-};
+//   return {
+//     props: {
+//       products,
+//     },
+//   };
+// };
