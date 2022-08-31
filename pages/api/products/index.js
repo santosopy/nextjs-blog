82f41f66545693ce63f1ded2a1dce7b2f5537a32
santosopy <santoso@tsart.jp>
2022-08-31 10:25:58 +0900

    pool

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 const getProducts = async (req, res) => {
   try {
     const results = await pool.query("SELECT * FROM product");
-    console.log(results)
+    console.log(pool)
     return res.status(200).json(results);
   } catch (error) {
     return res.status(500).json({ error });
