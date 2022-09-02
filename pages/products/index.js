773b4d2c64c523a35b844779030c3d0a71cdc46d
santosopy <santoso@tsart.jp>
2022-09-02 17:20:17 +0900

    up

--- pages/products/index.js
+++ pages/products/index.js
@@ -3,16 +3,17 @@ import { Layout } from "components/Layout";
 import { ProductCard } from "components/ProductCard";
 
 function ProductsPage({ products = [] }) {
-  // const renderProducts = () => {
-  //   if (products.length === 0) return <h1>No Products</h1>;
-  //   return products.map((product) => (
-  //     <ProductCard key={product.id} product={product} />
-  //   ));
-  // };
-  console.log(products)
+  const renderProducts = () => {
+    if (products.length === 0) return <h1>No Products</h1>;
+    return products.map((product) => (
+      <ProductCard key={product.id} product={product} />
+    ));
+  };
+
   return (
     <Layout>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-4">
+        {renderProducts()}
       </div>
     </Layout>
   );
@@ -24,6 +25,7 @@ export const getServerSideProps = async () => {
   const { data: products } = await axios.get(
     "https://nextjs-blog-flame-kappa-86.vercel.app/api/products"
   );
+    console.log(products)
   return {
     props: {
       products,
