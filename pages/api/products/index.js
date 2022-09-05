322f66fe5788c366592ef79b487a326c1b30ce7d
santosopy <santoso@tsart.jp>
2022-09-05 17:23:17 +0900

    ass

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,6 +1,7 @@
 import { pool } from "config/db";
 
 export default async function handler(req, res) {
+  console.log(res.statusCode)
   switch (req.method) {
     case "GET":
       return await getProducts(req, res);
@@ -12,7 +13,6 @@ export default async function handler(req, res) {
 }
 
 const getProducts = async (req, res) => {
-  console.log(res)
   try {
     const results = await pool.query("SELECT * FROM product");
     return res.status(200).json(results)
